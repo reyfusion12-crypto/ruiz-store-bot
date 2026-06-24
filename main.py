c61451df8b9e2eb3c6b8f1882f811db36cddc516
reyfusion12-crypto <reyfusion12@gmail.com>
2026-06-24 18:49:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: Message):
-
     await create_user(
         message.from_user.id,
         message.from_user.username,
@@ -23,29 +22,9 @@ async def start(message: Message):
     user = await get_user(message.from_user.id)
 
     await message.answer(
-        f"🛒 Bienvenido\n\n💰 Saldo: {user['balance']}",
+        f"🛒 Bienvenido\n💰 Saldo: {user['balance']}",
         reply_markup=main_menu
     )
-
-
-@dp.message()
-async def handler(message: Message):
-
-    user = await get_user(message.from_user.id)
-
-    if message.text == "👤 Perfil":
-        await message.answer(
-            f"👤 Perfil\n\nID: {user['id']}\nSaldo: {user['balance']}"
-        )
-
-    elif message.text == "💳 Recargar":
-        await message.answer("Envía comprobante al admin.")
-
-    elif message.text == "🛒 Tienda":
-        await message.answer("Tienda en construcción.")
-
-    elif message.text == "💰 Billetera":
-        await message.answer(f"Saldo: {user['balance']}")
 
 
 async def main():
