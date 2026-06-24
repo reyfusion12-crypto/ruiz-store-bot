ba9c28acd93fe400ba54fb5bc7db0d2dd9a19a71
reyfusion12-crypto <reyfusion12@gmail.com>
2026-06-24 18:48:12 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+
+
+import os
+
 BOT_TOKEN = "8678993710:AAFf84KlsCTbiKd_pVbNbw5NexGxER3sfhc"
 ADMINS = [5329713401]
 
-DATABASE_URL = "postgresql://postgres:PASS@reseau.proxy.rlwy.net:29268/railway"
+DATABASE_URL = os.getenv("DATABASE_URL")
